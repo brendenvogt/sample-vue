05a79d5e13b761747710f22ff963b302684302ab
Brenden Vogt <bvogtdev@gmail.com>
2021-10-21 02:50:04 -0700

    Added prop to footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{newuser}}</p>
+        <input v-model="newuser">
+    </div>
+</template>
+
+<script>
+// <Footer user="Hello" />
+export default {
+  props: ['user'],
+  data() {
+    return {
+      newuser: this.user,
+    };
+  },
+};
+</script>
