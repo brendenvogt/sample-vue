b9169c79e005562b0c5336858fe911034e311611
Brenden Vogt <bvogtdev@gmail.com>
2021-10-21 02:49:52 -0700

    Added prop to footer sample

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,21 @@
     <Header/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Footer user="Hello" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
   components: {
     HelloWorld,
     Header,
+    Footer,
   },
 })
 export default class Home extends Vue {}
